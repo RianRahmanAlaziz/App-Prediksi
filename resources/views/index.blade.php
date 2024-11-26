80e3abad53dd3523809927bddf719ed55554a8ea
Endang <endang@radian.office>
2024-11-26 15:48:30 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Prediksi
+
+    </title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/-Fixed-Navbar-start-with-transparency-background-BS4--transparency-menu.css">
+</head>
+<style>
+    .custom-card {
+        background-color: rgba(255, 255, 255, 0.5);
+        /* Transparansi pada card */
+    }
+
+    .custom-card input,
+    .custom-card select {
+        background-color: rgba(255, 255, 255, 0.5);
+        /* Transparansi pada input dan select */
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        /* Border agar elemen terlihat */
+    }
+</style>
+
+<body>
+    <div>
+        <header>
+            <nav class="navbar navbar-expand-lg  fixed-top bg-white transparency border-bottom  border-light navbar-light"
+                id="transmenu">
+                <div class="container">
+                    <a class="navbar-brand text-success " href="#header">
+                        <i class="fa fa-leaf fa-2x"></i>&nbsp;YOUR COMPANY
+                    </a>
+                    <button data-bs-toggle="collapse" class="navbar-toggler collapsed bg-dark"
+                        data-bs-target="#navcol-1"><span></span><span></span><span></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"><a class="nav-link text-dark" href="#">First Item</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="bg-secondary d-flex justify-content-center align-items-center"
+                style="background:url('/assets/img/background.png'); background-size:cover; min-height: 100vh;">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-sm-8 col-md-6">
+                            <div class="card bg-transparent" id="shadow">
+                                <h5 class="card-header text-center">Prediksi</h5>
+                                <div class="card-body">
+                                    <h5 class="card-title">Special title treatment</h5>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional
+                                        content.</p>
+                                    <div class="input-group  mb-3">
+                                        <select class="form-select bg-transparent" aria-label="Default select example">
+                                            <option selected>Open this select menu</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                    <label for="basic-url" class="form-label">Your vanity URL</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control bg-transparent" id="basic-url"
+                                            aria-describedby="basic-addon3">
+                                    </div>
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a href="#" class="btn btn-outline-dark">Submit</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/-Fixed-Navbar-start-with-transparency-background-BS4--transparency-menu.js"></script>
+</body>
+
+</html>
